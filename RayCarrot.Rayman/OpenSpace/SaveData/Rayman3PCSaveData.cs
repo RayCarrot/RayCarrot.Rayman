d39dd88ee546f39c789939cfa6ef2a8595f02714
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-05-05 17:15:19 +0200

    Parsed more R3 save data values, added DirectX key enum

--- RayCarrot.Rayman/OpenSpace/SaveData/Rayman3PCSaveData.cs
+++ RayCarrot.Rayman/OpenSpace/SaveData/Rayman3PCSaveData.cs
@@ -2,7 +2,6 @@
 
 namespace RayCarrot.Rayman.OpenSpace
 {
-    // WIP: There are more values in the save file than this (button mapping, current level score etc.) - figure out what they are for
     // WIP: Support other platforms - the format seems very similar
 
     /// <summary>
@@ -25,10 +24,21 @@ namespace RayCarrot.Rayman.OpenSpace
         /// </summary>
         public Rayman3PCSaveDataLevel[] Levels { get; set; }
 
+        public uint ForKeyAlgo { get; set; }
+        public uint Dword_7FE504 { get; set; }
+        public uint Dword_7FE508 { get; set; }
+        public uint Dword_7FE50C { get; set; }
+        public bool EndOfGameIsDone { get; set; }
+        public uint MaxIndCurrentEnv { get; set; }
+
+        // Always 0 and gets set to 0 when saving
+        public uint UnusedDword1 { get; set; }
+        public uint UnusedDword2 { get; set; }
+
         /// <summary>
-        /// Unknown values
+        /// Dummy byte for aligning the bytes, always 0
         /// </summary>
-        public byte[] Unknown1 { get; set; }
+        public byte DummyByte { get; set; }
 
         /// <summary>
         /// Indicates if controller vibration is enabled
@@ -45,41 +55,46 @@ namespace RayCarrot.Rayman.OpenSpace
         /// </summary>
         public bool IsVerticalInversionEnabled { get; set; }
 
-        /// <summary>
-        /// Unknown values
-        /// </summary>
-        public byte[] Unknown2 { get; set; }
+        public DirectXKey[] KeyboardMapping { get; set; }
 
-        /// <summary>
-        /// Same as <see cref="TotalScore"/>. This value is not always set.
-        /// </summary>
-        public int TotalScore2 { get; set; }
+        public ushort[] ControllerMapping { get; set; }
 
-        /// <summary>
-        /// Unknown value
-        /// </summary>
-        public int Unknown4 { get; set; }
+        // These are probably booleans (0 or 1)
+        public int UserWarnedAboutAutoSave { get; set; }
 
-        /// <summary>
-        /// Unknown value
-        /// </summary>
-        public int Unknown5 { get; set; }
+        public int RevisitMode { get; set; }
+        
+        public int RevisitEnvScoreAtBeginingOfLevel { get; set; }
+
+        public int RevisitGlobalScoreAtBeginingOfLevel { get; set; }
+
+        public int RevisitIndCurrentEnv { get; set; }
+
+        public int RevisitIndEnvironementFromWhereWeCome { get; set; }
 
         /// <summary>
         /// The current level to load, or "endgame" if the game has been finished
         /// </summary>
-        public string CurrentLevel { get; set; }
+        public string LevelNameToSave { get; set; }
 
-        //// Offset 212
-        ///// <summary>
-        ///// The last unlocked level, only set if it differs from <see cref="CurrentLevel"/>
-        ///// </summary>
-        //public string LastLevel { get; set; }
+        public string LevelNameAfterReinitToSave { get; set; }
 
-        /// <summary>
-        /// Unknown values
-        /// </summary>
-        public byte[] Unknown3 { get; set; }
+        public byte[] Unk1 { get; set; }
+
+        public int SetVolumeSound { get; set; }
+
+        public int SetVolumeVoice { get; set; }
+
+        public int SetVolumeMusic { get; set; }
+        
+        public int SetVolumeAmbiance { get; set; }
+        
+        public int SetVolumeMenu { get; set; }
+
+        // Contains data like the current player health etc.
+        public Rayman3PCSaveDataEntry[] DataEntries { get; set; }
+
+        public byte[] Unk2 { get; set; }
 
         /// <summary>
         /// Handles the serialization using the specified serializer
@@ -92,24 +107,44 @@ namespace RayCarrot.Rayman.OpenSpace
 
             Levels = s.SerializeObjectArray<Rayman3PCSaveDataLevel>(Levels, 9, name: nameof(Levels));
 
-            Unknown1 = s.SerializeArray<byte>(Unknown1, 33, name: nameof(Unknown1));
+            ForKeyAlgo = s.Serialize<uint>(ForKeyAlgo, name: nameof(ForKeyAlgo));
+            Dword_7FE504 = s.Serialize<uint>(Dword_7FE504, name: nameof(Dword_7FE504));
+            Dword_7FE508 = s.Serialize<uint>(Dword_7FE508, name: nameof(Dword_7FE508));
+            Dword_7FE50C = s.Serialize<uint>(Dword_7FE50C, name: nameof(Dword_7FE50C));
+            EndOfGameIsDone = s.SerializeBool<uint>(EndOfGameIsDone, name: nameof(EndOfGameIsDone));
+            MaxIndCurrentEnv = s.Serialize<uint>(MaxIndCurrentEnv, name: nameof(MaxIndCurrentEnv));
+            UnusedDword1 = s.Serialize<uint>(UnusedDword1, name: nameof(UnusedDword1));
+            UnusedDword2 = s.Serialize<uint>(UnusedDword2, name: nameof(UnusedDword2));
 
-            // TODO: As they formatted as ints?
+            DummyByte = s.Serialize<byte>(DummyByte, name: nameof(DummyByte));
             IsVibrationEnabled = s.Serialize<bool>(IsVibrationEnabled, name: nameof(IsVibrationEnabled));
             IsHorizontalInversionEnabled = s.Serialize<bool>(IsHorizontalInversionEnabled, name: nameof(IsHorizontalInversionEnabled));
             IsVerticalInversionEnabled = s.Serialize<bool>(IsVerticalInversionEnabled, name: nameof(IsVerticalInversionEnabled));
 
-            Unknown2 = s.SerializeArray<byte>(Unknown2, 64, name: nameof(Unknown2));
+            KeyboardMapping = s.SerializeArray<DirectXKey>(KeyboardMapping, 13, name: nameof(KeyboardMapping));
+            ControllerMapping = s.SerializeArray<ushort>(ControllerMapping, 13, name: nameof(ControllerMapping));
 
-            TotalScore2 = s.Serialize<int>(TotalScore2, name: nameof(TotalScore2));
+            UserWarnedAboutAutoSave = s.Serialize<int>(UserWarnedAboutAutoSave, name: nameof(UserWarnedAboutAutoSave));
+            RevisitMode = s.Serialize<int>(RevisitMode, name: nameof(RevisitMode));
+            RevisitEnvScoreAtBeginingOfLevel = s.Serialize<int>(RevisitEnvScoreAtBeginingOfLevel, name: nameof(RevisitEnvScoreAtBeginingOfLevel));
+            RevisitGlobalScoreAtBeginingOfLevel = s.Serialize<int>(RevisitGlobalScoreAtBeginingOfLevel, name: nameof(RevisitGlobalScoreAtBeginingOfLevel));
+            RevisitIndCurrentEnv = s.Serialize<int>(RevisitIndCurrentEnv, name: nameof(RevisitIndCurrentEnv));
+            RevisitIndEnvironementFromWhereWeCome = s.Serialize<int>(RevisitIndEnvironementFromWhereWeCome, name: nameof(RevisitIndEnvironementFromWhereWeCome));
 
-            Unknown4 = s.Serialize<int>(Unknown4, name: nameof(Unknown4));
-            Unknown5 = s.Serialize<int>(Unknown5, name: nameof(Unknown5));
+            LevelNameToSave = s.SerializeString(LevelNameToSave, 20, LevelNameToSave);
+            LevelNameAfterReinitToSave = s.SerializeString(LevelNameAfterReinitToSave, 20, LevelNameAfterReinitToSave);
 
-            // TODO: The length is probably fixed?
-            CurrentLevel = s.Serialize<string>(CurrentLevel, name: nameof(CurrentLevel));
+            Unk1 = s.SerializeArray<byte>(Unk1, 16, name: nameof(Unk1));
 
-            Unknown3 = s.SerializeArray<byte>(Unknown3, (int)(s.Stream.Length - s.Stream.Position), name: nameof(Unknown3));
+            SetVolumeSound = s.Serialize<int>(SetVolumeSound, name: nameof(SetVolumeSound));
+            SetVolumeVoice = s.Serialize<int>(SetVolumeVoice, name: nameof(SetVolumeVoice));
+            SetVolumeMusic = s.Serialize<int>(SetVolumeMusic, name: nameof(SetVolumeMusic));
+            SetVolumeAmbiance = s.Serialize<int>(SetVolumeAmbiance, name: nameof(SetVolumeAmbiance));
+            SetVolumeMenu = s.Serialize<int>(SetVolumeMenu, name: nameof(SetVolumeMenu));
+
+            DataEntries = s.SerializeObjectArray<Rayman3PCSaveDataEntry>(DataEntries, 3, name: nameof(DataEntries));
+
+            Unk2 = s.SerializeArray<byte>(Unk2, (int)(s.Stream.Length - s.Stream.Position), name: nameof(Unk2));
         }
     }
 }
---
+++ RayCarrot.Rayman/OpenSpace/SaveData/Rayman3PCSaveDataEntry.cs
@@ -0,0 +1,36 @@
+﻿using RayCarrot.Binary;
+
+namespace RayCarrot.Rayman.OpenSpace
+{
+    /// <summary>
+    /// Data entry in a Rayman 3 PC save file
+    /// </summary>
+    public class Rayman3PCSaveDataEntry : IBinarySerializable
+    {
+        /// <summary>
+        /// The length of the key
+        /// </summary>
+        public byte KeyLength { get; set; }
+
+        /// <summary>
+        /// The key
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// The data
+        /// </summary>
+        public byte[] Data { get; set; }
+
+        /// <summary>
+        /// Handles the serialization using the specified serializer
+        /// </summary>
+        /// <param name="s">The serializer</param>
+        public void Serialize(IBinarySerializer s)
+        {
+            KeyLength = s.Serialize<byte>(KeyLength, name: nameof(KeyLength));
+            Key = s.SerializeString(Key, KeyLength, name: nameof(Key));
+            Data = s.SerializeArray<byte>(Data, 6, name: nameof(Data));
+        }
+    }
+}
