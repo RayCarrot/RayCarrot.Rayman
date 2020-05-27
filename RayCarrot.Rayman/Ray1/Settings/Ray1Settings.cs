e9db90a2bd92ba7cec2d9b84d81b34ab779b29d2
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-05-27 09:07:42 +0200

    Normalized R1 serializer settings

---
+++ RayCarrot.Rayman/Ray1/Settings/Ray1Settings.cs
@@ -0,0 +1,33 @@
+﻿using RayCarrot.Binary;
+using System.Text;
+
+namespace RayCarrot.Rayman.Ray1
+{
+    /// <summary>
+    /// Binary settings for serializing Rayman 1 game formats
+    /// </summary>
+    public class Ray1Settings : BinarySerializerSettings
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="endian">The endianness</param>
+        /// <param name="textEncoding">The text encoding to use</param>
+        public Ray1Settings(Endian endian, Encoding textEncoding) : base(endian, textEncoding)
+        { }
+
+        #endregion
+
+        #region Public Static Methods
+
+        /// <summary>
+        /// Gets the default settings
+        /// </summary>
+        /// <returns>The settings</returns>
+        public static Ray1Settings GetDefaultSettings() => new Ray1Settings(Endian.Little, Encoding.GetEncoding(437));
+
+        #endregion
+    }
+}
