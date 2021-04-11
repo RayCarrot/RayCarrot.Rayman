e8a01c61e2cd4350fd1cc51b47b7270c317707b7
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-04-11 20:24:19 +0200

    Name R1 config values

--- RayCarrot.Rayman/Ray1/SaveData/Rayman1PCConfigData.cs
+++ RayCarrot.Rayman/Ray1/SaveData/Rayman1PCConfigData.cs
@@ -9,72 +9,53 @@ namespace RayCarrot.Rayman.Ray1
     {
         public R1Languages Language { get; set; }
 
-        public uint Dword_01 { get; set; }
-        public uint Dword_05 { get; set; }
-        public uint Dword_09 { get; set; }
-        public uint Dword_0D { get; set; }
-        public uint Dword_11 { get; set; }
-        public byte Byte_15 { get; set; }
-        public ushort Word_16 { get; set; }
-        public ushort Word_18 { get; set; }
-        public ushort Word_1A { get; set; }
-        public ushort Word_1C { get; set; }
+        public uint Port { get; set; }
+        public uint Irq { get; set; }
+        public uint Dma { get; set; }
+        public uint Param { get; set; }
+        public uint DeviceID { get; set; }
+        public byte NumCard { get; set; }
 
-        public ushort MusicEnabled { get; set; }
-        
-        // 0-20
-        public ushort SoundVolume { get; set; }
+        public ushort KeyJump { get; set; } // Keys are 0-3
+        public ushort KeyWeapon { get; set; }
+        public ushort Options_jeu_10 { get; set; }
+        public ushort KeyAction { get; set; }
 
-        public ushort Word_22 { get; set; }
+        public ushort MusicCdActive { get; set; } // Options_jeu_12
+        public ushort VolumeSound { get; set; } // Options_jeu_13, set as (127 * value / 20), 0-20
+
+        public ushort Options_jeu_14 { get; set; }
         public byte Kit_Byte_23 { get; set; }
         public byte Kit_Byte_24 { get; set; }
-        public byte Byte_24 { get; set; }
-        public byte Byte_25 { get; set; }
+        public byte Mode_Pad { get; set; }
+        public byte Port_Pad { get; set; }
 
-        public ushort JoystickMaxX { get; set; }
-        public ushort JoystickMinX { get; set; }
-        public ushort JoystickMaxY { get; set; }
-        public ushort JoystickMinY { get; set; }
-        public ushort JoystickMiddleX { get; set; }
-        public ushort JoystickMiddleY { get; set; }
+        public ushort XPadMax { get; set; }
+        public ushort XPadMin { get; set; }
+        public ushort YPadMax { get; set; }
+        public ushort YPadMin { get; set; }
+        public ushort XPadCentre { get; set; }
+        public ushort YPadCentre { get; set; }
 
-        public byte Byte_32 { get; set; }
-        public byte Byte_33 { get; set; }
-        public byte Byte_34 { get; set; }
-        public byte Byte_35 { get; set; }
+        public byte[] NotBut { get; set; }
+        public byte[] Tab_Key { get; set; } // Left, up, right, down, jump, fist, action
 
-        public byte Button_Left { get; set; }
-        public byte Button_Up { get; set; }
-        public byte Button_Right { get; set; }
-        public byte Button_Down { get; set; }
-        public byte Button_Jump { get; set; }
-        public byte Button_Fist { get; set; }
-        public byte Button_Action { get; set; }
+        public byte GameModeVideo { get; set; } // pci1 = 0, pci2 = 1
+        public byte P486 { get; set; } // pci1 or pci2 = 0, vesa = 1
+        public byte SizeScreen { get; set; } // 4/4 = 0, 3/4 = 1, 2/4 = 2, 1/4 = 3
+        public byte Frequence { get; set; } // "60" = 0, "50" = 1, "max" = 2
 
-        // pci1 = 0, pci2 = 1
-        public byte GraphicsMode { get; set; }
+        public bool FixOn { get; set; } // Scores enabled
+        public bool BackgroundOptionOn { get; set; }
+        public bool ScrollDiffOn { get; set; }
 
-        // pci1 or pci2 = 0, vesa = 1
-        public byte VesaMode { get; set; }
-
-        // 4/4 = 0, 3/4 = 1, 2/4 = 2, 1/4 = 3
-        public byte ZoneOfPlay { get; set; }
-
-        // "60" = 0, "50" = 1, "max" = 2
-        public byte FrequencyMode { get; set; }
-
-        public bool ScoresEnabled { get; set; }
-
-        public bool BackgroundsEnabled { get; set; }
-        public bool ParallaxScrollingEnabled { get; set; }
-
-        public byte[] Bytes_44 { get; set; }
-        public byte[] Bytes_54 { get; set; }
-        public byte[] Bytes_64 { get; set; }
-        public uint Dword_74 { get; set; }
-        public uint Dword_78 { get; set; }
-        public uint Dword_7C { get; set; }
-        public uint Dword_80 { get; set; }
+        public byte[] RefRam2VramNormalFix { get; set; }
+        public byte[] RefRam2VramNormal { get; set; }
+        public byte[] RefTransFondNormal { get; set; }
+        public uint RefSpriteNormal { get; set; }
+        public uint RefRam2VramX { get; set; }
+        public uint RefVram2VramX { get; set; }
+        public uint RefSpriteX { get; set; }
 
         /// <summary>
         /// Handles the serialization using the specified serializer
@@ -88,21 +69,21 @@ namespace RayCarrot.Rayman.Ray1
             if (settings.Game == Ray1Game.Rayman1)
                 Language = s.Serialize<R1Languages>(Language, name: nameof(Language));
 
-            Dword_01 = s.Serialize<uint>(Dword_01, name: nameof(Dword_01));
-            Dword_05 = s.Serialize<uint>(Dword_05, name: nameof(Dword_05));
-            Dword_09 = s.Serialize<uint>(Dword_09, name: nameof(Dword_09));
-            Dword_0D = s.Serialize<uint>(Dword_0D, name: nameof(Dword_0D));
-            Dword_11 = s.Serialize<uint>(Dword_11, name: nameof(Dword_11));
-            Byte_15 = s.Serialize<byte>(Byte_15, name: nameof(Byte_15));
-            Word_16 = s.Serialize<ushort>(Word_16, name: nameof(Word_16));
-            Word_18 = s.Serialize<ushort>(Word_18, name: nameof(Word_18));
-            Word_1A = s.Serialize<ushort>(Word_1A, name: nameof(Word_1A));
-            Word_1C = s.Serialize<ushort>(Word_1C, name: nameof(Word_1C));
+            Port = s.Serialize<uint>(Port, name: nameof(Port));
+            Irq = s.Serialize<uint>(Irq, name: nameof(Irq));
+            Dma = s.Serialize<uint>(Dma, name: nameof(Dma));
+            Param = s.Serialize<uint>(Param, name: nameof(Param));
+            DeviceID = s.Serialize<uint>(DeviceID, name: nameof(DeviceID));
+            NumCard = s.Serialize<byte>(NumCard, name: nameof(NumCard));
+            KeyJump = s.Serialize<ushort>(KeyJump, name: nameof(KeyJump));
+            KeyWeapon = s.Serialize<ushort>(KeyWeapon, name: nameof(KeyWeapon));
+            Options_jeu_10 = s.Serialize<ushort>(Options_jeu_10, name: nameof(Options_jeu_10));
+            KeyAction = s.Serialize<ushort>(KeyAction, name: nameof(KeyAction));
 
-            MusicEnabled = s.Serialize<ushort>(MusicEnabled, name: nameof(MusicEnabled));
-            SoundVolume = s.Serialize<ushort>(SoundVolume, name: nameof(SoundVolume));
+            MusicCdActive = s.Serialize<ushort>(MusicCdActive, name: nameof(MusicCdActive));
+            VolumeSound = s.Serialize<ushort>(VolumeSound, name: nameof(VolumeSound));
 
-            Word_22 = s.Serialize<ushort>(Word_22, name: nameof(Word_22));
+            Options_jeu_14 = s.Serialize<ushort>(Options_jeu_14, name: nameof(Options_jeu_14));
 
             if (settings.Game != Ray1Game.Rayman1)
             {
@@ -110,45 +91,35 @@ namespace RayCarrot.Rayman.Ray1
                 Kit_Byte_24 = s.Serialize<byte>(Kit_Byte_24, name: nameof(Kit_Byte_24));
             }
 
-            Byte_24 = s.Serialize<byte>(Byte_24, name: nameof(Byte_24));
-            Byte_25 = s.Serialize<byte>(Byte_25, name: nameof(Byte_25));
+            Mode_Pad = s.Serialize<byte>(Mode_Pad, name: nameof(Mode_Pad));
+            Port_Pad = s.Serialize<byte>(Port_Pad, name: nameof(Port_Pad));
 
-            JoystickMaxX = s.Serialize<ushort>(JoystickMaxX, name: nameof(JoystickMaxX));
-            JoystickMinX = s.Serialize<ushort>(JoystickMinX, name: nameof(JoystickMinX));
-            JoystickMaxY = s.Serialize<ushort>(JoystickMaxY, name: nameof(JoystickMaxY));
-            JoystickMinY = s.Serialize<ushort>(JoystickMinY, name: nameof(JoystickMinY));
-            JoystickMiddleX = s.Serialize<ushort>(JoystickMiddleX, name: nameof(JoystickMiddleX));
-            JoystickMiddleY = s.Serialize<ushort>(JoystickMiddleY, name: nameof(JoystickMiddleY));
+            XPadMax = s.Serialize<ushort>(XPadMax, name: nameof(XPadMax));
+            XPadMin = s.Serialize<ushort>(XPadMin, name: nameof(XPadMin));
+            YPadMax = s.Serialize<ushort>(YPadMax, name: nameof(YPadMax));
+            YPadMin = s.Serialize<ushort>(YPadMin, name: nameof(YPadMin));
+            XPadCentre = s.Serialize<ushort>(XPadCentre, name: nameof(XPadCentre));
+            YPadCentre = s.Serialize<ushort>(YPadCentre, name: nameof(YPadCentre));
 
-            Byte_32 = s.Serialize<byte>(Byte_32, name: nameof(Byte_32));
-            Byte_33 = s.Serialize<byte>(Byte_33, name: nameof(Byte_33));
-            Byte_34 = s.Serialize<byte>(Byte_34, name: nameof(Byte_34));
-            Byte_35 = s.Serialize<byte>(Byte_35, name: nameof(Byte_35));
+            NotBut = s.SerializeArray<byte>(NotBut, 4, name: nameof(NotBut));
+            Tab_Key = s.SerializeArray<byte>(Tab_Key, 7, name: nameof(Tab_Key));
 
-            Button_Left = s.Serialize<byte>(Button_Left, name: nameof(Button_Left));
-            Button_Up = s.Serialize<byte>(Button_Up, name: nameof(Button_Up));
-            Button_Right = s.Serialize<byte>(Button_Right, name: nameof(Button_Right));
-            Button_Down = s.Serialize<byte>(Button_Down, name: nameof(Button_Down));
-            Button_Jump = s.Serialize<byte>(Button_Jump, name: nameof(Button_Jump));
-            Button_Fist = s.Serialize<byte>(Button_Fist, name: nameof(Button_Fist));
-            Button_Action = s.Serialize<byte>(Button_Action, name: nameof(Button_Action));
+            GameModeVideo = s.Serialize<byte>(GameModeVideo, name: nameof(GameModeVideo));
+            P486 = s.Serialize<byte>(P486, name: nameof(P486));
+            SizeScreen = s.Serialize<byte>(SizeScreen, name: nameof(SizeScreen));
+            Frequence = s.Serialize<byte>(Frequence, name: nameof(Frequence));
+            FixOn = s.Serialize<bool>(FixOn, name: nameof(FixOn));
+            BackgroundOptionOn = s.Serialize<bool>(BackgroundOptionOn, name: nameof(BackgroundOptionOn));
+            ScrollDiffOn = s.Serialize<bool>(ScrollDiffOn, name: nameof(ScrollDiffOn));
 
-            GraphicsMode = s.Serialize<byte>(GraphicsMode, name: nameof(GraphicsMode));
-            VesaMode = s.Serialize<byte>(VesaMode, name: nameof(VesaMode));
-            ZoneOfPlay = s.Serialize<byte>(ZoneOfPlay, name: nameof(ZoneOfPlay));
-            FrequencyMode = s.Serialize<byte>(FrequencyMode, name: nameof(FrequencyMode));
-            ScoresEnabled = s.Serialize<bool>(ScoresEnabled, name: nameof(ScoresEnabled));
-            BackgroundsEnabled = s.Serialize<bool>(BackgroundsEnabled, name: nameof(BackgroundsEnabled));
-            ParallaxScrollingEnabled = s.Serialize<bool>(ParallaxScrollingEnabled, name: nameof(ParallaxScrollingEnabled));
+            RefRam2VramNormalFix = s.SerializeArray<byte>(RefRam2VramNormalFix, 0x10, name: nameof(RefRam2VramNormalFix));
+            RefRam2VramNormal = s.SerializeArray<byte>(RefRam2VramNormal, 0x10, name: nameof(RefRam2VramNormal));
+            RefTransFondNormal = s.SerializeArray<byte>(RefTransFondNormal, 0x10, name: nameof(RefTransFondNormal));
 
-            Bytes_44 = s.SerializeArray<byte>(Bytes_44, 0x10, name: nameof(Bytes_44));
-            Bytes_54 = s.SerializeArray<byte>(Bytes_54, 0x10, name: nameof(Bytes_54));
-            Bytes_64 = s.SerializeArray<byte>(Bytes_64, 0x10, name: nameof(Bytes_64));
-
-            Dword_74 = s.Serialize<uint>(Dword_74, name: nameof(Dword_74));
-            Dword_78 = s.Serialize<uint>(Dword_78, name: nameof(Dword_78));
-            Dword_7C = s.Serialize<uint>(Dword_7C, name: nameof(Dword_7C));
-            Dword_80 = s.Serialize<uint>(Dword_80, name: nameof(Dword_80));
+            RefSpriteNormal = s.Serialize<uint>(RefSpriteNormal, name: nameof(RefSpriteNormal));
+            RefRam2VramX = s.Serialize<uint>(RefRam2VramX, name: nameof(RefRam2VramX));
+            RefVram2VramX = s.Serialize<uint>(RefVram2VramX, name: nameof(RefVram2VramX));
+            RefSpriteX = s.Serialize<uint>(RefSpriteX, name: nameof(RefSpriteX));
         }
     }
 }
